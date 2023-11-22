1e80e2f0ac313ca2557d95a164392b95a08edf34
unknown <you@example.com>
2023-11-22 08:37:49 -0300

    aplicando linha com toggleClass

--- Desktop/EBAC - Curso TI do zero ao pro/Profissão Desenvolvedor Full Stack Java/exercicio/exercicio-modulo9/main.js
+++ Desktop/EBAC - Curso TI do zero ao pro/Profissão Desenvolvedor Full Stack Java/exercicio/exercicio-modulo9/main.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
-    let riscaTarefa = ("none");
-    
-    
+
     $('form').on('submit', function(e){
         e.preventDefault();
         
         const campoNomeTarefa = $('#novaTarefa').val();
-        
-        $(`<li><button id="botao-lista" type="button" style="text-decoration: ${riscaTarefa}">${campoNomeTarefa}</button></li>`).appendTo('ol');
-        
-        $('#botao-lista').click(function(){
-            riscaTarefa = ("line-through")
-        })
+        const novoItem = $('');
+
+        $(`<li>${campoNomeTarefa}</li>`).appendTo('ul');
         $('#novaTarefa').val('');
-    })
+    });
+
+    $('ul').on('click','li',function(){
+        $(this).toggleClass('risca');
+    });
     
 })
